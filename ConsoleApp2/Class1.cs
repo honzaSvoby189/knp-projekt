8ac7d1e16a3e27770564d53928d60ca789471ec6
jan.svoboda <jan.svoboda@VT3-16>
2024-10-23 13:39:21 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kámen_nužky_papír
+{
+    internal class Hra
+    {
+        public int SkoreHrac { get; private set; }
+        public int SkoreNepritele { get; private set; }
+
+        public Hra()
+        {
+            SkoreHrac = 0;
+            SkoreNepritele = 0;
+        }
+
+        public void ZvysitSkoreHrace()
+        {
+            SkoreHrac++;
+        }
+
+        public void ZvysitSkoreNepritele()
+        {
+            SkoreNepritele++;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,72 @@
+﻿using kámen_nužky_papír;
+
+string[] moznosti = { "1", "2", "3" };
+Hra hra = new Hra();
+
+Console.WriteLine("Hra kámen, nůžky, papír");
+Console.WriteLine("-------------------------");
+
+while (hra.SkoreHrac < 5 && hra.SkoreNepritele < 5)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Skóre - Hráč: " + hra.SkoreHrac + " | Počítač: " + hra.SkoreNepritele);
+    Console.WriteLine("-------------------------");
+
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("kolo hráče");
+    Console.WriteLine("1-kámen");
+    Console.WriteLine("2-nůžky");
+    Console.WriteLine("3-papír");
+    Console.WriteLine("----------------------------------");
+    string zadanaHodnota = Console.ReadLine().ToLower();
+    Console.Clear();
+
+    if (!Array.Exists(moznosti, element => element.Equals(zadanaHodnota)))
+    {
+        Console.WriteLine("Neplatný výběr.");
+        continue;
+    }
+
+    Random random = new Random();
+    int volbaProtiHraceIndex = random.Next(moznosti.Length);
+    string PC = moznosti[volbaProtiHraceIndex];
+
+
+    Console.WriteLine("Počítač vybral: " + PC);
+    Console.WriteLine("Vybral jste: " + zadanaHodnota);
+    Console.WriteLine("------------------------------------");
+
+
+    if (zadanaHodnota == PC)
+    {
+        Console.WriteLine("Remíza!");
+    }
+    else if (
+        (zadanaHodnota == "1" && PC == "2") ||
+        (zadanaHodnota == "2" && PC == "3") ||
+        (zadanaHodnota == "3" && PC == "1")
+        )
+    {
+        Console.WriteLine("Vyhráli jste!");
+        hra.ZvysitSkoreHrace();
+    }
+    else
+    {
+        Console.WriteLine("Počítač vyhrál!");
+        hra.ZvysitSkoreNepritele();
+    }
+    Console.WriteLine("-----------------");
+
+
+}
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+if (hra.SkoreHrac == 5)
+{
+    Console.WriteLine("Vyhráli jsi!!");
+}
+else
+{
+    Console.WriteLine("Proti hráč vyhrál !!!! jsi celelkem hovno ngl  ");
+}
